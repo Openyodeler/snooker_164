e325016378d745defe0886c7402f6495221e4ec7
Openyodeler <talaytalay15@gmail.com>
2026-07-25 12:01:22 +0700

    Ballspawn

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 using UnityEditor.ShaderGraph;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -22,6 +23,7 @@ public class Ball : MonoBehaviour , IPointerClickHandler
     [SerializeField] private MeshRenderer meshRenderer;
     [SerializeField] private Material[] ballMaterials;
 
+
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log(point);
@@ -43,5 +45,21 @@ public class Ball : MonoBehaviour , IPointerClickHandler
 
         }
 
+    }
+
+    public void colorthis(BallColor ballColor)
+    {   
+        color = ballColor;
+        point = (int)color;
+        if (meshRenderer != null && ballMaterials != null)
+        {
+            int index = (int)color;
+
+            if (index >= 0 && index < ballMaterials.Length && ballMaterials[index] != null)
+            {
+                meshRenderer.sharedMaterial = ballMaterials[index];
+            }
+
+        }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,18 +6,36 @@ public class GameManager : MonoBehaviour
 
     public int PlayerScore { get { return playerScore; } set { playerScore = value; } }
     public static GameManager instance;
+
+    [SerializeField] private GameObject[] ballPositions;
+    [SerializeField] private GameObject ballPrefab;
     private void Awake()
     {
         instance = this;
     }
     void Start()
     {
-        
+        int i = 0;
+        foreach (var ball in ballPositions)
+        {   
+            SetBall((BallColor)i, i);
+            i++;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SetBall(BallColor col,int i)
+    {
+        GameObject obj = Instantiate(ballPrefab,
+                    ballPositions[i].transform.position,
+                    Quaternion.identity);
+
+        Ball b = obj.GetComponent<Ball>();
+        b.colorthis(col);
     }
 }
